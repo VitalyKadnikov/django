60a4a7b722d77668fd8d26850d4a60e0a63c08eb
VitalyKadnikov <40638180+VitalyKadnikov@users.noreply.github.com>
2019-07-19 12:47:15 +0300

    Add files via upload

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,27 +1,25 @@
-from django.shortcuts import render
-import json, os
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
 
-JSON_PATH = 'mainapp/json'
-
-def loadMenuFromJSON():
-    with open(os.path.join(JSON_PATH, 'menu.json'), 'r') as infile:
-        return json.load(infile)
 
 def main(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'username':'Vitaly', 'array':[1, 2, 3, 4, 5]}
+    context = {'username': 'Vitaly'}
     return render(request, 'mainapp/main.html', context)
 
-def products(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu,
-               'products': Product.objects.all(),
-               'categories': ProductCategory.objects.all(),
-               }
+
+def products(request, pk=None):
+    products = Product.objects.all()
+    basket = []
+    if request.user:
+        basket = request.user.basket.all()
+
+    if pk:
+        category = get_object_or_404(ProductCategory, pk=pk)
+        products = products.filter(category=category)
+
+    context = {'products': products, 'categories': ProductCategory.objects.all(), 'basket': basket}
     return render(request, 'mainapp/products.html', context)
 
+
 def contacts(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu}
-    return render(request, 'mainapp/contacts.html', context)
+    return render(request, 'mainapp/contacts.html')
