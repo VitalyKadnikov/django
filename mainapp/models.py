16e87b929e8e206162f16e41ddbf08568b155773
VitalyKadnikov <40638180+VitalyKadnikov@users.noreply.github.com>
2019-07-20 21:21:47 +0300

    Add files via upload

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-    name = models.CharField(verbose_name='имя категории', max_length=255)
-    desc = models.TextField(verbose_name='Описание категории', blank=True)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     class Meta:
@@ -16,10 +21,11 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
-    short_desc = models.CharField(verbose_name='краткое описание продукта', max_length=60, blank=True)
-    description = models.TextField(verbose_name='описание продукта', blank=True)
-    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+    short_desc = models.CharField(verbose_name='кратко', max_length=60, blank=True)
+    description = models.TextField(verbose_name='подробно', blank=True)
+    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='склад', default=0)
+    is_hot = models.BooleanField(verbose_name='горячий продукт', default=False)
 
     def __str__(self):
-        return f"{self.name} ({self.category.name})"
+        return "{} ({})".format(self.name, self.category.name)
